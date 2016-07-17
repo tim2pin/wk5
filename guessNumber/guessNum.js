744ca6855ed8921764a2303613f2d1559656a8f3
Tim Toupin <tim2pin@gmail.com>
2016-07-17 18:59:27 -0500

    updates

---
+++ guessNumber/guessNum.js
@@ -0,0 +1,17 @@
+var numToGuess = Math.floor(Math.random()*100 + 1);
+console.log(numToGuess);
+
+
+function yourGuess() {
+    guess = document.getElementById("guess").value;
+    guesses = document.getElementById("output");
+
+    if (guess == numToGuess) {
+        guesses.value = guesses.value + "\r" + "You have guessed correctly! ("+guess+")";
+    } else if (guess > numToGuess) {
+        guesses.value = guesses.value + "\r" + "Youre guessing too high!("+guess+")";
+    } else {
+        guesses.value = guesses.value + "\r" + "Youre guessing too low!("+guess+")";
+    }
+}
+    
